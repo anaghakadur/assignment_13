e6b7fac04b90b14dc3a12f5b4444f20ca4d96dcb
anaghakadur <akadurashwatha1914@sdsu.edu>
2023-12-14 21:52:55 -0800

    Final

--- server/server.js
+++ server/server.js
@@ -1,80 +1,82 @@
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser')
-var mongoose = require('mongoose')
-var cors = require('cors')
-require('dotenv').config();
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var app = express();
 
-const dbURL = process.env.dbURL
+app.use(bodyParser.json());
 
-app.use(express.static(__dirname))
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+var dbPath = 'mongodb+srv://anagha:Nodeproject@nodeproject.wrljirc.mongodb.net/';
 
-// parse application/json
-app.use(bodyParser.json())
 
-app.use(cors())
+var Product = mongoose.model('product', {
+    product: {
+        productid: Number,
+        category: String,
+        price: String,
+        name: String,
+        instock: Boolean
+    },
+    id : Number
+});
 
-const Product = mongoose.model('products', {
-  productid: { type: Number, default: Date.now },
-  category: { type: String, default: '' },
-  price: { type: Number, default: 0 },
-  name: { type: String, default: '' },
-  instock: {type: Boolean, default: true}
-})
+mongoose.connect(dbPath, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}, (err) => {
+    console.log("Mongodb connection", err);
+});
 
-app.get('/products/get', (req, res) => {
-  Product.find({}, (error, products) => {
-    res.send(products)
-  })
-})
+/**
+ * Get All products from the database
+ */
+app.get('/product/get/', async (req, res) => {
+    var products = {};
+    var data = await Product.find({});
+    data.forEach((value) => {
+        products[value.id] = value.product;
+    });
+    res.send(products);
+});
 
-app.post('/products/create', (req, res) => {
-  console.log("Creating product");
-  const product = new Product(req.body)
-  product.save((error) => {
-    if(error) {
-      res.sendStatus(500)
+/**
+ * Create and Insert a product to database
+ */
+app.post('/product/create/', async (req, res) => {
+    try{
+        var product = new Product(req.body);
+        await product.save();
+        console.log("Saved");
+        res.sendStatus(200);
+
+    }catch (error) {
+        res.sendStatus(500);
     }
-    else {
-      res.sendStatus(200)
-    }
-  })
-})
+});
 
-app.post('/products/delete/:productId', (req, res) => {
-  const productId = req.params.productId || ''
-  Product.deleteOne({productid: productId}, (error) => {
-    if(error) {
-      res.sendStatus(500)
+/**
+ * Update given product
+ */
+app.put('/product/update/:id', async (req, res) => {
+    try{
+        var reqestId = req.params.id;
+        console.log(req.body)
+        await Product.findOneAndUpdate({id: reqestId}, req.body);
+        res.sendStatus(200);   
+    }catch (error) {
+        console.log(error);
+        res.sendStatus(500);
     }
-    else {
-      res.sendStatus(200)
-    }
-  })
-})
 
-app.post('/products/update/:productId', (req, res) => {
-  const productId = req.params.productId || ''
-  //console.log('To be updated product id ' + productId)
-  const product = new Product(req.body)
-  //console.log('Requested update ' + JSON.stringify(req.body))
-  Product.updateOne({productid: productId}, req.body, (error) => {
-    if(error) {
-      console.log(error)
-      res.sendStatus(500)
-    }
-    else {
-      res.sendStatus(200)
-    }
-  })
-})
 
-mongoose.connect(dbURL, {useNewUrlParser: true, useUnifiedTopology: true}, (error) => {
-  console.log('MongoDB database connection', error)
-})
+});
 
-var server = app.listen(3000, () => {
-  console.log('Server is listening on the port', server.address().port)
-})
+/**
+ * Delete the given product
+ */
+app.delete('/product/delete/:id', async (req, res) => {
+    var reqestId = req.params.id;
+    await Product.findOneAndDelete({id: reqestId});
+    res.sendStatus(200);
+});
+
+
+app.listen(4000, () => {
+    console.log('server is listening in 4000');
+});
